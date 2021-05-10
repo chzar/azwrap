4c813a9897aefbeb44a0e42204c55e81d3989c4d
Cameron Harrell <cameron.harrell@bouvet.no>
2021-05-10 09:38:28 +0200

    Refactored exceptions, new example in readme

--- src/azwrap/exceptions.py
+++ src/azwrap/exceptions.py
@@ -1,18 +1,35 @@
-class AzRequestError(Exception):
-    pass
-
-class AzParserError(Exception):
-    pass
-
-class AzResourceNotFoundError(Exception):
-    pass
-
-class AzUnsupportedCommand(Exception):
-    pass
-
-class AzCommandSyntaxError(Exception):
-    def __init__(self, stderr: str):
+class AzError(Exception):
+    def __init__(self, stdout:str, stderr: str):
+        self.__stdout = stdout
         self.__stderr = stderr
 
-    def __str__(self) -> str:
+    @property
+    def stdout(self):
+        return self.__stdout
+
+    @property
+    def stderr(self):
         return self.__stderr
+    
+    def __str__(self) -> str:
+        return self.stderr
+
+class AzRequestError(AzError):
+    def __init__(self, stdout: str, stderr: str):
+        super().__init__(stdout, stderr)
+
+class AzParserError(AzError):
+    def __init__(self, stdout: str, stderr: str):
+        super().__init__(stdout, stderr)
+
+class AzResourceNotFoundError(AzError):
+    def __init__(self, stdout: str, stderr: str):
+        super().__init__(stdout, stderr)
+
+class AzUnsupportedCommand(AzError):
+    def __init__(self, stdout: str, stderr: str):
+        super().__init__(stdout, stderr)
+
+class AzCommandSyntaxError(AzError):
+    def __init__(self, stdout: str, stderr: str):
+        super().__init__(stdout, stderr)
