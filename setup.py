a4b10922cd2fff31b1862e31c2b1bd17f0c238a0
Cameron Harrell <cameron.harrell@bouvet.no>
2021-05-10 09:55:26 +0200

    set version to 0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='azwrap',
-    version='0.2',
+    version='0.3',
     package_dir = {'': 'src'},
     packages = find_packages(where='src'),
     install_requires = ['pyfakefs']
