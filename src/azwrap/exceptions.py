a691c3f1cf9daaf32e16eee4835a3edf27e27ca4
chzar <cameron.harrell@bouvet.no>
2021-05-13 22:43:16 +0200

    added original command list to exception output

--- src/azwrap/exceptions.py
+++ src/azwrap/exceptions.py
@@ -1,7 +1,10 @@
+from types import List
+
 class AzError(Exception):
-    def __init__(self, stdout:str, stderr: str):
+    def __init__(self, stdout:str, stderr: str, commands: List(str)):
         self.__stdout = stdout
         self.__stderr = stderr
+        self.__commands = commands
 
     @property
     def stdout(self):
@@ -11,25 +14,29 @@ class AzError(Exception):
     def stderr(self):
         return self.__stderr
     
+    @property
+    def commands(self):
+        return self.__commands
+
     def __str__(self) -> str:
-        return self.stderr
+        return f'[commands]: {self.commands}\n[stderr]:\n{self.stderr}'
 
 class AzRequestError(AzError):
-    def __init__(self, stdout: str, stderr: str):
-        super().__init__(stdout, stderr)
+    def __init__(self, stdout: str, stderr: str, commands: List(str)):
+        super().__init__(stdout, stderr, commands)
 
 class AzParserError(AzError):
-    def __init__(self, stdout: str, stderr: str):
-        super().__init__(stdout, stderr)
+    def __init__(self, stdout: str, stderr: str, commands: List(str)):
+        super().__init__(stdout, stderr, commands)
 
 class AzResourceNotFoundError(AzError):
-    def __init__(self, stdout: str, stderr: str):
-        super().__init__(stdout, stderr)
+    def __init__(self, stdout: str, stderr: str, commands: List(str)):
+        super().__init__(stdout, stderr, commands)
 
 class AzUnsupportedCommand(AzError):
-    def __init__(self, stdout: str, stderr: str):
-        super().__init__(stdout, stderr)
+    def __init__(self, stdout: str, stderr: str, commands: List(str)):
+        super().__init__(stdout, stderr, commands)
 
 class AzCommandSyntaxError(AzError):
-    def __init__(self, stdout: str, stderr: str):
-        super().__init__(stdout, stderr)
+    def __init__(self, stdout: str, stderr: str, commands: List(str)):
+        super().__init__(stdout, stderr, commands)
